ec8ea51ed6f42b181ef77d7a023ce3188edc9403
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-23 20:11:16 +0900

    Redirect HTTP to HTTPS

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Force HTTPS.
+app.use((req, res, next) => {
+  if(req.hostname !== "localhost" && req.get("X-Forwarded-Proto") !== "https") {
+    res.redirect("https://" + req.host + req.originalUrl);
+  } else {
+    next();
+  }
+});
+
+// Serve Static Files.
 app.use(express.static("static"));
 
 app.listen(PORT, () => {
